5ba56f29c01d602a2d7fbfbd03bebdd4153b71e7
cleutonsampaio <cleuton.sampaio@gmail.com>
2013-12-11 08:48:16 -0200

    Finally could create and run an IT test!

---
+++ jqana/src/it/first-it/src/main/java/com/obomprogramador/tools/jqana_test/it_project_1/Teste.java
@@ -0,0 +1,59 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterException;
+import java.util.List;
+
+public class Teste {private static final int PAGE_EXISTS = 0;
+private static final int NO_SUCH_PAGE = 0;
+private List<String> relatorio;
+
+public int print(Graphics g, PageFormat pf, int pagina)
+		throws PrinterException {
+	int retorno = PAGE_EXISTS;
+	int totalPaginas = relatorio.size() / 30; //int totalPaginas = relatorio.size() / 30;
+	if (relatorio.size() % 30 > 0) {
+		totalPaginas++;
+	}
+   try {
+	   System.out.println("OK");
+   }
+   catch (Exception ex) {
+	   totalPaginas = 0;
+   }
+   
+   // while
+   /*
+   if
+   */
+   int teste = retorno == 0 ? 100 : (retorno == NO_SUCH_PAGE) ? 10 : -1;
+   /*
+    * if (retorno == 0) {
+    * 	teste = 100;
+    * }
+    * else if (retorn0 == NO_SUCH_PAGE {
+    * 			teste = -1;
+    * 	   }
+    */
+   
+
+   String xpto = "Teste ? ";	
+   if (pagina > totalPaginas) {
+		retorno = NO_SUCH_PAGE;
+	}
+	else {
+		int inicial = pagina * 30;
+		int contaLinhas = 0;
+		Graphics2D g2d = (Graphics2D)g;
+	    g2d.translate(pf.getImageableX(), pf.getImageableY());
+		for (int x = inicial; x < relatorio.size(); x++) {
+			contaLinhas++;
+			if (contaLinhas > 30 || contaLinhas > 20) {
+				break;
+			}
+			g.drawString(relatorio.get(x), 40, 50 + contaLinhas * 14);
+		}
+	}
+	return retorno;
+}
+}
---
+++ jqana/src/test/resources/TesteLcomMaiorQueUm.java
@@ -0,0 +1,57 @@
+package abc;
+/*
+ * LCOM4 = 2
+ */
+public class TesteLcomMaiorQueUm {
+
+	private int [] z;
+	private int x;
+	private int y;
+	
+	public TesteLcomMaiorQueUm() {
+
+	}
+	
+	public int getX() {
+		return x;
+		
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void metodoA() {
+		System.out.println( metodoB () + 1);
+	}
+	
+	public int metodoB() {
+		int a = x + 2 * x;
+		return a;
+	}
+	
+	public void metodoC() {
+		y = 1;
+	}
+	
+	public void metodoD() {
+		setY(1);
+		if ( metodoE () < 0 ) {
+			System.out.println("ERRO");
+		}
+	}
+	
+	public int metodoE() {
+		return 10;
+		
+	}
+
+}
